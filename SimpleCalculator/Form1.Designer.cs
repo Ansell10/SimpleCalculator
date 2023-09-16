649127c000b608c6d51fa17873a8f49383cf9693
Ansell10 <syomeronansell@gmail.com>
2023-09-16 19:45:23 +0700

    change form name

--- SimpleCalculator/Form1.Designer.cs
+++ SimpleCalculator/Form1.Designer.cs
@@ -178,7 +178,7 @@
             Controls.Add(menuStrip1);
             MainMenuStrip = menuStrip1;
             Name = "Form1";
-            Text = "Form1";
+            Text = "SimpleCalculator";
             menuStrip1.ResumeLayout(false);
             menuStrip1.PerformLayout();
             ResumeLayout(false);
